cd8bc2adbbcd1a2e4594bce9c675d5c3c37cdf46
MiLee777 <midyashka1506@gmail.com>
2025-12-17 20:40:22 +1100

    init: first commit

---
+++ src/api/api.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:3000/api/v1',
+});
+
+export const getCurrency = () => {
+  return api.get('/currency');
+};
+
+export const getHistory = () => {
+  return api.get('/history');
+};
